d3e810b9a92213888e3c865d1c6711715cad3760
Aron Carroll <self@aroncarroll.com>
2014-04-28 11:24:48 +0200

    Wrap the code in an UMD wrapper

--- microevent.js
+++ microevent.js
@@ -1,45 +1,46 @@
-function MicroEvent(){}
+// Universal Module Definition: http://git.io/J7vtzw
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.returnExports = factory();
+    }
+}(this, function () {
+    function MicroEvent(){}
 
-MicroEvent.prototype = {
-    constructor: MicroEvent,
-    bind: function(event, fct){
-        this._events = this._events || {};
-        this._events[event] = this._events[event] || [];
-        this._events[event].push(fct);
-    },
-    unbind: function(event, fct){
-        this._events = this._events || {};
-        if(event in this._events === false) { return; }
-        this._events[event].splice(this._events[event].indexOf(fct), 1);
-    },
-    trigger: function(event /* , args... */){
-        this._events = this._events || {};
-        if (event in this._events === false) { return; }
-        for(var i = 0; i < this._events[event].length; i++){
-            this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
+    MicroEvent.prototype = {
+        constructor: MicroEvent,
+        bind: function(event, fct){
+            this._events = this._events || {};
+            this._events[event] = this._events[event] || [];
+            this._events[event].push(fct);
+        },
+        unbind: function(event, fct){
+            this._events = this._events || {};
+            if(event in this._events === false) { return; }
+            this._events[event].splice(this._events[event].indexOf(fct), 1);
+        },
+        trigger: function(event /* , args... */){
+            this._events = this._events || {};
+            if (event in this._events === false) { return; }
+            for(var i = 0; i < this._events[event].length; i++){
+                this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
+            }
+        }
+    };
+
+    MicroEvent.mixin = function(destObject){
+        var props = ['bind', 'unbind', 'trigger'];
+        for(var i = 0; i < props.length; i ++){
+            if( typeof destObject === 'function' ){
+                destObject.prototype[props[i]] = MicroEvent.prototype[props[i]];
+            }else{
+                destObject[props[i]] = MicroEvent.prototype[props[i]];
+            }
         }
     }
-};
 
-/**
- * mixin will delegate all MicroEvent.js function in the destination object
- *
- * - require('MicroEvent').mixin(Foobar) will make Foobar able to use MicroEvent
- *
- * @param {Object} the object which will support MicroEvent
-*/
-MicroEvent.mixin = function(destObject){
-    var props = ['bind', 'unbind', 'trigger'];
-    for(var i = 0; i < props.length; i ++){
-        if( typeof destObject === 'function' ){
-            destObject.prototype[props[i]] = MicroEvent.prototype[props[i]];
-        }else{
-            destObject[props[i]] = MicroEvent.prototype[props[i]];
-        }
-    }
-}
-
-// export in common js
-if( typeof module !== "undefined" && ('exports' in module)){
-    module.exports  = MicroEvent;
-}
+    return MicroEvent;
+}));
