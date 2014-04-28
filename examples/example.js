e0a4b2fe458f309ecbb77084bb77a36d6eae2b89
Aron Carroll <self@aroncarroll.com>
2014-04-28 11:41:50 +0200

    Clean up the examples

--- examples/example.js
+++ examples/example.js
@@ -1,14 +1,14 @@
-// import microevent.js 
-var MicroEvent	= require('../microevent-debug.js')
+// import microevent.js
+var MicroEvent = require('../microevent.js')
 
 /**
  * Ticker is a class periodically sending out dummy tick events
 */
-var Ticker	= function( interval ){
-	var self	= this;
-	setInterval(function(){
-		self.trigger('tick', new Date());
-	}, 1000);
+var Ticker = function (interval) {
+    var self = this;
+    setInterval(function () {
+        self.trigger('tick', new Date());
+    }, 1000);
 };
 /**
  * make Ticker support MicroEventjs
@@ -18,9 +18,9 @@ MicroEvent.mixin(Ticker);
 // create a ticker
 var ticker = new Ticker();
 // bind the 'tick' event
-ticker.bind('tick', function(date) {
-	// display to check
-	console.log('notified date', date);
+ticker.bind('tick', function (date) {
+    // display to check
+    console.log('notified date', date);
 });
 
 
